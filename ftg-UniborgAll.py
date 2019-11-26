57eeabb2b50e9a41c9f1c1d5fca967843c3259a7
rebel6969 <57802851+rebel6969@users.noreply.github.com>
2019-11-26 19:15:16 +0530

    Update ftg-UniborgAll.py

--- ftg-UniborgAll.py
+++ ftg-UniborgAll.py
@@ -339,3 +339,28 @@ async def _(event):
         await asyncio.sleep(animation_interval)
 
         await event.edit(animation_chars[i % 11])
+
+"""Self Destruct Plugin
+.sd <time in seconds> <text>
+"""
+
+
+import time
+
+from telethon.errors import rpcbaseerrors
+from uniborg.util import admin_cmd
+import importlib.util
+
+
+
+@borg.on(admin_cmd("sd", outgoing=True  ))
+async def selfdestruct(destroy):
+    """ For .sd command, make seflf-destructable messages. """
+    if not destroy.text[0].isalpha() and destroy.text[0] not in ("/", "#", "@", "!"):
+        message = destroy.text
+        counter = int(message[4:6])
+        text = str(destroy.text[6:])
+        await destroy.delete()
+        smsg = await destroy.client.send_message(destroy.chat_id, text)
+        time.sleep(counter)
+        await smsg.delete()
