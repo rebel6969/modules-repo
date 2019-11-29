0ca51a19a601e37511d87b6e2503a4ecdcf56e5c
rebel6969 <57802851+rebel6969@users.noreply.github.com>
2019-11-29 19:19:16 +0530

    Update help.py

--- help.py
+++ help.py
@@ -46,42 +46,42 @@ class HelpMod(loader.Module):
         
     async def helpcmd(self, message):
         """Block this user to PM without being warned"""
-        raph = "<b>Available Commands in RaphielGang section:<b>\n"
-        uni = "<b>Available Commands in Uniborg section:<b>\n"
-        ftg = "<b>Available Commands in Friendly-Telegram section:</b>\n"
+        raph = "<b>Available Commands in RaphielGang section:<b>\n<b>—–—–--------------–——–––—--–——–—–—–——–––—–—–———</b>\n<i>To unload</i><code>.unloadmod RaphielGang</code><i>(num)</i>"
+        uni = "<b>Available Commands in Uniborg section:<b>\n<b>—–—–--------------–——–––—–——–—--–—–——–––—–—–———</b>\n<i>To unload</i><code>.unloadmod UniBorg</code><i>(num)</i>"
+        ftg = "<b>Available Commands in Friendly-Telegram section:</b>\n<b>—–—–----------------–——–––—–——–—–—–——–––—–—–———</b>\n<i>To unload</i><code>.unloadmod</code><i>modulename</i>"
         all = "<b>Available Commands:</b>\n"
         args = utils.get_args_raw(message)
         if not args:
         	await message.edit(
-        	"<b>Please choose a section to see your modules:</b>"
-        	"\n\n\n<b>.help raph:</b><i> Shows all your PP and PPE originated modules.</i>\n"
-        	"\n<b>.help uni:</b><i> Shows all your Uniborg originated modules.</i>\n"
-        	"\n<b>.help ftg:</b><i> Shows all your native modules.</i>\n"
-        	"\n<b>.help all:</b><i> Shows all your modules.</i>")
+        	"<b>Please choose a section to see your modules:</b>\n<b>—–—–--------------–——–––—–——–—–—–——–––—–—–———</b>"
+        	"\n<code>.help raph</code>:<i> Shows all your PP and PPE originated modules.</i>\n"
+        	"\n<code>.help uni</code>:<i> Shows all your Uniborg originated modules.</i>\n"
+        	"\n<code>.help ftg</code>:<i> Shows all your native modules.</i>\n"
+        	"\n<code>.help all</code>:<i> Shows all your modules.</i>")
         	return
         for mod in self.allmodules.modules:
         	if mod.name.find("RaphielGang") != -1:
         		raph += "\n\n<b>" + mod.name + "</b>: "
         		for cmd in mod.commands:
-        			raph += "<i>" + cmd + "</i>, "
+        			raph += "<code>." + cmd + "</code>, "
         		raph = raph[:-2:]
         		raph = raph.replace("Raphielgang Configuration Placeholder:", "")
         	if mod.name.find("UniBorg") != -1:
         		uni += "\n\n<b>" + mod.name + "</b>: "
         		for cmd in mod.commands:
-        			uni += "<i>" + cmd + "</i>, "
+        			uni += "<code>." + cmd + "</code>, "
         		uni = uni[:-2:]
         		uni = uni.replace("Uniborg Configuration Placeholder:", "")
         	if mod.name.find("UniBorg") == -1 and mod.name.find("RaphielGang") == -1:
         		ftg += "\n\n<b>" + mod.name + "</b>: "
         		for cmd in mod.commands:
-        			ftg += "<i>" + cmd + "</i>, "
+        			ftg += "<code>." + cmd + "</code>, "
         		ftg = ftg[:-2:]
         		ftg = ftg.replace("Raphielgang Configuration Placeholder:", "")
         		ftg = ftg.replace("Uniborg Configuration Placeholder:", "")
         	all += "\n\n<b>" + mod.name + "</b>: "
         	for cmd in mod.commands:
-        		all += "<i>" + cmd + "</i>, "
+        		all += "<code>." + cmd + "</code>, "
         	all = all[:-2:]
         if args == "uni":
         	await message.edit(uni)
