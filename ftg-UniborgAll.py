d33976c6449e54218e5ac9c77b6e5cdf7c17d199
rebel6969 <57802851+rebel6969@users.noreply.github.com>
2019-12-03 22:21:52 +0530

    Update ftg-UniborgAll.py

--- ftg-UniborgAll.py
+++ ftg-UniborgAll.py
@@ -364,3 +364,53 @@ async def selfdestruct(destroy):
         smsg = await destroy.client.send_message(destroy.chat_id, text)
         time.sleep(counter)
         await smsg.delete()
+
+
+"""command: .hack"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.rape (.*)", outgoing=True))
+
+async def fuck(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 2
+
+    animation_ttl = range(0, 11)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "rape":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+        
+            "`Targeting Rape Victim...`",
+            "`Victim Selected.`",
+            "`Raping... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Raping... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Raping... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
+            "`Raping... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Raping... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Raping... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Raping... 84%\n█████████████████████▒▒▒▒ `",
+            "`Raping... 100%\n█████████RAPED███████████ `",
+            "`Target Raped Successfully...\n\nPay 999$ To @RebelVicious and get Raped`"
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 11])
