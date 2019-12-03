92a9f03e45f3881e0ae2ea8b92c4edf1a198a06a
rebel6969 <57802851+rebel6969@users.noreply.github.com>
2019-12-03 22:51:30 +0530

    Update ftg-UniborgAll.py

--- ftg-UniborgAll.py
+++ ftg-UniborgAll.py
@@ -410,3 +410,74 @@ async def _(event):
         await asyncio.sleep(animation_interval)
 
         await event.edit(animation_chars[i % 11])
+
+	
+"""Emoji
+
+Available Commands:
+
+.repe
+
+build by legend @r4v4n4 , if u edit it then u r gay..."""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=".repe", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.3
+
+    animation_ttl = range(0, 30)
+
+
+
+    animation_chars = [
+        
+            "**r**",
+            "**ra**",
+            "**rap**",
+            "**rape**",
+            "**rape_**",    
+            "**rape_t**",
+            "**rape_tr**",
+            "**rape_tra**",
+            "**rape_trai**",
+            "**rape_train**",
+            "**ape_train🚅**",
+            "**pe_train🚅🚃🚃**",
+            "**e_train🚅🚃🚃🚃**",
+            "**_train🚅🚃🚃🚃🚃**",
+            "**train🚅🚃🚃🚃🚃🚃**",
+            "**rain🚅🚃🚃🚃🚃🚃🚃**",
+            "**ain🚅🚃🚃🚃🚃🚃🚃🚃**",
+            "**in🚅🚃🚃🚃🚃🚃🚃🚃🚃**",
+            "**n🚅🚃🚃🚃🚃🚃🚃🚃🚃🚃**",
+            "🚅🚃🚃🚃🚃🚃🚃🚃🚃🚃",
+            "🚃🚃🚃🚃🚃🚃🚃🚃🚃",
+            "🚃🚃🚃🚃🚃🚃🚃🚃",
+            "🚃🚃🚃🚃🚃🚃🚃",
+            "🚃🚃🚃🚃🚃🚃",
+            "🚃🚃🚃🚃🚃",
+            "🚃🚃🚃🚃",
+            "🚃🚃🚃",
+            "🚃🚃",
+            "🚃",
+            "**rApEd**"
+ ]
+
+    for i in animation_ttl:
+
+        await asyncio.sleep(animation_interval)
+
+        await event.edit(animation_chars[i % 30])
