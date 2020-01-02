5053030e8cc4c98925be14f4c4303c4075cb1545
rebel6969 <57802851+rebel6969@users.noreply.github.com>
2020-01-02 18:04:03 +0530

    Update song.py

--- song.py
+++ song.py
@@ -25,7 +25,7 @@ async def _(event):
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("Searching For Song...")
+    await event.edit("**Searching For Song...**")
     bruh(str(cmd))
     lol = glob.glob("*.mp3")
     loa = lol[0]
