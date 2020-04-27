6297425f7dfc2f94d9f779cd52bbe3a2f26ca6d4
rebel6969 <57802851+rebel6969@users.noreply.github.com>
2020-04-27 15:18:00 +0530

    Update heroku.py

--- heroku.py
+++ heroku.py
@@ -18,89 +18,10 @@ from telethon.tl.types import MessageMediaPhoto
 import glob
 
 class Var:
-    HEROKU_APP_NAME="young-inlet-89534"
     HEROKU_API_KEY="f1072845-0058-4b28-9f01-928b3f651110"
 
 Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
-
-
-@register(outgoing=True, pattern=r"^\.(set|get|del) var(?: |$)(.*)(?: |$)([\s\S]*)")
-async def variable(var):
-    """
-        Manage most of ConfigVars setting, set new var, get current var,
-        or delete var...
-    """
-    if Var.HEROKU_APP_NAME is not None:
-        app = Heroku.app(Var.HEROKU_APP_NAME)
-    else:
-        return await var.edit("`[HEROKU]:"
-                              "\nPlease setup your` **HEROKU_APP_NAME**")
-    exe = var.pattern_match.group(1)
-    heroku_var = app.config()
-    if exe == "get":
-        await var.edit("`Getting information...`")
-        await asyncio.sleep(1.5)
-        try:
-            variable = var.pattern_match.group(2).split()[0]
-            if variable in heroku_var:
-                return await var.edit("**ConfigVars**:"
-                                      f"\n\n`{variable} = {heroku_var[variable]}`\n")
-            else:
-                return await var.edit("**ConfigVars**:"
-                                      f"\n\n`Error:\n-> {variable} don't exists`")
-        except IndexError:
-            configs = prettyjson(heroku_var.to_dict(), indent=2)
-            with open("configs.json", "w") as fp:
-                fp.write(configs)
-            with open("configs.json", "r") as fp:
-                result = fp.read()
-                if len(result) >= 4096:
-                    await var.client.send_file(
-                        var.chat_id,
-                        "configs.json",
-                        reply_to=var.id,
-                        caption="`Output too large, sending it as a file`",
-                    )
-                else:
-                    await var.edit("`[HEROKU]` ConfigVars:\n\n"
-                                   "================================"
-                                   f"\n```{result}```\n"
-                                   "================================"
-                                   )
-            os.remove("configs.json")
-            return
-    elif exe == "set":
-        await var.edit("`Setting information...`")
-        variable = var.pattern_match.group(2)
-        if not variable:
-            return await var.edit(">`.set var <ConfigVars-name> <value>`")
-        value = var.pattern_match.group(3)
-        if not value:
-            variable = variable.split()[0]
-            try:
-                value = var.pattern_match.group(2).split()[1]
-            except IndexError:
-                return await var.edit(">`.set var <ConfigVars-name> <value>`")
-        await asyncio.sleep(1.5)
-        if variable in heroku_var:
-            await var.edit(f"**{variable}**  `successfully changed to`  ->  **{value}**")
-        else:
-            await var.edit(f"**{variable}**  `successfully added with value`  ->  **{value}**")
-        heroku_var[variable] = value
-    elif exe == "del":
-        await var.edit("`Getting information to deleting variable...`")
-        try:
-            variable = var.pattern_match.group(2).split()[0]
-        except IndexError:
-            return await var.edit("`Please specify ConfigVars you want to delete`")
-        await asyncio.sleep(1.5)
-        if variable in heroku_var:
-            await var.edit(f"**{variable}**  `successfully deleted`")
-            del heroku_var[variable]
-        else:
-            return await var.edit(f"**{variable}**  `is not exists`")
-
 
 
 @register(outgoing=True, pattern="^.usage(?: |$)(.*)")
