d7d5a438df99173b512304c665e28a26032b42f0
rebel6969 <57802851+rebel6969@users.noreply.github.com>
2020-05-12 13:32:20 +0530

    Update song.py

--- song.py
+++ song.py
@@ -4,27 +4,13 @@ import os
 import requests
 from userbot.events import register
 from telethon.tl.types import MessageMediaPhoto
-
 import glob
-           
-                      
 import youtube_dl
-              
-                                                             
-
- 
-                           
-                                   
-                                               
-
-
 
 
 def bruh(name):
     lc = 'youtube-dl --extract-audio --audio-format mp3 "ytsearch:'+  name+'"'
-    
     os.system(lc)
-
     
 @register(outgoing=True, pattern="^.song(?: |$)(.*)")
 async def _(event):
@@ -40,11 +26,10 @@ async def _(event):
     lol = glob.glob("*.mp3")
     loa = lol[0]
     xx = await event.client.upload_file(loa,part_size_kb=512)
-    await event.edit("done")
+    await event.delete()
     await event.client.send_file(
                 event.chat_id,
                 xx,
-                                 
                 caption=loa,
                 force_document=True,reply_to=reply_to_id) 
     os.system("rm -rf *.mp3")
